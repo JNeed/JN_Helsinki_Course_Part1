e2f9324d8883a89ce4da37b1b44208e0bb339773
JNeed <joshua.needleman510@gmail.com>
2023-07-05 13:42:19 -0400

    Update App.js
    Made course object that contains the name and parts. Changed parts to be an array of objects.

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -7,7 +7,6 @@ const Header = (props) => {
 } 
 
 const Part = (props) => {
-  console.log('Entered Part component');
   return (
   <div>
     <p>{props.partName} {props.numExercises}</p>
@@ -18,29 +17,35 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part partName = {props.parts[props.index]} numExercises = {props.totals[props.index]} />
+      <Part partName = {props.parts[0].name} numExercises = {props.parts[0].exercises} />
+      <Part partName = {props.parts[1].name} numExercises = {props.parts[1].exercises} />
+      <Part partName = {props.parts[2].name} numExercises = {props.parts[2].exercises} />
     </div>
   );
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-  const parts = [part1, part2, part3];
-  const totals = [exercises1, exercises2, exercises3, exercises1+exercises2+exercises3];
-  const contentComponents = [];
-  for (let i =0; i < 3; i++){
-    contentComponents.push(<Content parts={parts} totals={totals} index={i} />)
-  }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name:'Fundamentals of React',
+      exercises:10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises:14
+    }
+    ]
+  };
   return (
     <div>
-      <Header course={course} />
-      {contentComponents}
+      <Header course={course.name} />
+      <Content parts={course.parts} />
     </div>
   );
 }
