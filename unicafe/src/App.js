bc8b1f7969a229874401d27eb9f96898924b49fd
JNeed <joshua.needleman510@gmail.com>
2023-07-07 17:46:13 -0400

    made survey app for unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [score, setScore] = useState(0)
+  const [numResponses, setNumResponses] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+    setScore(score+1)
+    setNumResponses(numResponses + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+    setScore(score-1)
+    setNumResponses(numResponses + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setNumResponses(numResponses + 1)
+  }
+
+  const calcAverage = () => {
+    let average = 0;
+    if (numResponses > 0) {
+      average = score / numResponses;
+    }
+    return (
+    <p>
+      Average: {average}
+    </p>)}
+
+    const calcPercentPositive = () => {
+      let percentPositive = 0;
+      if (numResponses > 0){
+        percentPositive = good / numResponses * 100;
+      }
+      return(
+        <p> Percent positive: {percentPositive}</p>
+      )
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Leave Feedback</h1>
+      <div><button onClick = {handleGood}> Good </button>
+      <button onClick = {handleBad}> Bad </button>
+      <button onClick = {handleNeutral}> Neutral </button>
+      </div>
+      <h2> Statistics </h2>
+      <p>Good {good}</p>
+      <p> Bad {bad}</p>
+      <p> Neutral {neutral}</p>
+      <p> Score {score}</p>
+      {calcAverage()}
+      {calcPercentPositive()}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
