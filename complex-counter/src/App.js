84afde4469de3002c9451c12f951c47c52247ea5
JNeed <joshua.needleman510@gmail.com>
2023-07-07 17:12:09 -0400

    created new app; removed some imports in index.js

--- complex-counter/src/App.js
+++ complex-counter/src/App.js
@@ -1,45 +1,46 @@
 import {useState} from 'react'
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  const [clicks, setClicks] = useState({
-    left:0,right:0
-  })
-  const [allClicks, setAll] = useState([])
 
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const Button = ({handleClick, text}) => {
+      return(<button onClick = {handleClick}>{text}</button>)
+  }
+  const History = (props) => {
+    if (props.allClicks.length === 0){
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
 
   const handleLeftClick = () => {
-  
-    const newClicks = {
-      ...clicks, // I think this technically expands to left:clicks.left, right:clicks.right, 
-      // left:clicks.left+1, but the first left:clicks.left doesn't do anything, it just sets the variable
-      // it its current value and then we increment it later with left:clicks.left+1
-      left: clicks.left+1
-      // right: clicks.right
-    }
-    setClicks(newClicks)
+    setLeft(left+1)
     setAll(allClicks.concat('L'))
+    setTotal(left+right)
   }
   const handleRightClick = () => {
-    const newClicks = {
-      // left:clicks.left,
-      ...clicks,
-      right:clicks.right+1
-    }
-    setClicks(newClicks)
+    setRight(right+1)
     setAll(allClicks.concat('R'))
+    setTotal(left+right)
   }
   return (
     <div>
-      {clicks.left}
-      <button onClick= {handleLeftClick}>
-        left
-      </button>
-      <button onClick = {handleRightClick}>
-        right
-      </button>
-      {clicks.right}
-      <p>{allClicks.join(' ')}</p>
+      {left}
+      <Button handleClick = {handleLeftClick} text='left' />
+      <Button handleClick = {handleRightClick} text='right' />
+      {right}
+      <History allClicks = {allClicks} />
     </div>
   )
     
